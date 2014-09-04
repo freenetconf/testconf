154992dd9e81d510bffbd5e4a0bf8dd69949b92e
Petar Koretic <petar.koretic@sartura.hr>
2014-09-04 14:26:50 +0200

    config: add config module
    Signed-off-by: Petar Koretic <petar.koretic@sartura.hr>

---
+++ core/config.js
@@ -0,0 +1,6 @@
+var config = {}
+module.exports = config
+
+config.yang_dir = __dirname + "/../yang/"
+config.keys_dir = __dirname + "/../keys/"
+config.server_methods_dir = __dirname + "/../netconf_server/methods/"
