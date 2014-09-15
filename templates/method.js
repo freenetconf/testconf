aceeb00107fc05cef387d04f49fc49c884dc0e17
Petar Koretic <petar.koretic@sartura.hr>
2014-09-15 17:18:19 +0200

    template: add template for code generation
    Signed-off-by: Petar Koretic <petar.koretic@sartura.hr>

---
+++ templates/method.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2014 Cisco Systems, Inc.
+ *
+ * Author: Petar Koretic <petar.koretic@sartura.hr>
+ * Author: Luka Perkov <luka.perkov@sartura.hr>
+ *
+ * testconf is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with testconf. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var netconf = require(__dirname + '/../../core/netconf')
+
+var methods = netconf.create_default_methods()
+
+module.exports = methods
+
