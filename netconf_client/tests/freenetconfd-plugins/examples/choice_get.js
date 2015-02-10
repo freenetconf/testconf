fee400cdc95fc16a24ab8f74a7159cf907037751
Zvonimir Fras <zvonimir.fras@sartura.hr>
2015-02-10 12:48:21 +0100

    freenetconfd: examples: add choice tests
    Signed-off-by: Zvonimir Fras <zvonimir.fras@sartura.hr>

---
+++ netconf_client/tests/freenetconfd-plugins/examples/choice_get.js
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2014 Cisco Systems, Inc.
+ *
+ * Author: Petar Koretic <petar.koretic@sartura.hr>
+ * Author: Luka Perkov <luka.perkov@sartura.hr>
+ *
+ * testconf is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with testconf. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var netconf_client = require('../../../netconf_client')
+
+var xml='<get><filter><player xmlns="urn:ietf:params:xml:ns:yang:choice"/></filter></get>'
+
+var client = netconf_client.create(function(error)
+{
+	if (error)
+	{
+		console.error(error)
+		process.exit(1)
+	}
+
+	client.send(xml, function(error, reply)
+	{
+		if (error)
+		{
+			console.error(error)
+			process.exit(1)
+		}
+
+		client.send_close(function(error, reply)
+		{
+			if (error)
+			{
+				console.error(error)
+				process.exit(1)
+			}
+			else
+			{
+				process.exit(0)
+			}
+
+		})
+	})
+})
+
+client.on('rpc-reply', function(reply)
+{
+})
+
+client.on('error', function(error)
+{
+	console.error(error)
+	process.exit(1)
+})
+
+client.on('end', function(error)
+{
+})
