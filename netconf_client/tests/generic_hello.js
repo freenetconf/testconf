61f840d44bcda43ebdd8e0838dc8d6b46b26a05e
Mak Krnic <mak.krnic@sartura.hr>
2016-03-03 15:50:12 +0100

    client: test: generic_hello: rewrite using promises
    Signed-off-by: Mak Krnic <mak.krnic@sartura.hr>

--- netconf_client/tests/generic_hello.js
+++ netconf_client/tests/generic_hello.js
@@ -15,38 +15,23 @@
 
 var netconf_client = require('../netconf_client')
 
-var client = netconf_client.create(function(error)
+var client = netconf_client.create().then(function(client)
 {
-	if (error)
+	client.send_close().then(function(success)
 	{
-		console.error(error)
+		console.log(success)
+		process.exit(0)
+	},
+	function(reject)
+	{
+		console.log(reject)
 		process.exit(1)
-	}
-
-	client.send_close(function(error, reply)
-	{
-		if (error)
-		{
-			console.error(error)
-			process.exit(1)
-		}
-		else
-		{
-			process.exit(0)
-		}
 	})
-})
-
-client.on('rpc-reply', function(error)
+},
+function(error)
 {
-})
-
-client.on('error', function(error)
-{
-	console.error(error)
 	process.exit(1)
 })
-
-client.on('end', function(error)
+.finally(function()
 {
 })
